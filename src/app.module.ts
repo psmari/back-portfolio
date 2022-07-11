1b81453ba7bf4ac55dc529055185bb07510fef1d
mariana.silva <mariana.silva@solucx.com.br>
2022-07-10 22:54:06 -0300

    feat: Create typeorm configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
+import { ProjectModule } from './project/project.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(typeOrmConfig),
+    ProjectModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_DATABASE,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  synchronize: true,
+  entities: ['./dist/**/**/*.entity{.js,.ts}'],
+};
