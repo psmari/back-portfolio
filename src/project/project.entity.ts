13f049483b75ca6af195d4bc3bf5b5134f035d69
mariana.silva <mariana.silva@solucx.com.br>
2022-07-10 22:55:27 -0300

    feat: Create project module

---
+++ src/project/project.entity.ts
@@ -0,0 +1,28 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'project' })
+export class Project {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column({ name: 'name', nullable: false })
+  name: string;
+
+  @Column({ name: 'link', nullable: true })
+  link: string;
+
+  @Column({ name: 'description', nullable: true })
+  description: string;
+
+  @CreateDateColumn({ name: 'create_at' })
+  createAt: string;
+
+  @UpdateDateColumn({ name: 'update_at' })
+  updateAt: string;
+}
